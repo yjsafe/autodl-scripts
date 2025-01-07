e38809e6adf3988dec1392e8866cbe58c9df577f
ryomahan <ryomahan1996@gmail.com>
2025-01-07 16:12:36 +0800

    fix: fix shell

---
+++ start-Qwen2.5-32B-Instruct-GPTQ-Int4.sh
@@ -0,0 +1,4 @@
+# /bin/bash
+
+cd autodl-tmp/models/
+nohup vllm serve Qwen2.5-32B-Instruct-GPTQ-Int4 --port 6006 --gpu-memory-utilization 0.98 --max-model-len 7000 > run.log 2>&1 &
--- start-qwen-32B.sh
+++
@@ -1,7 +0,0 @@
-# /bin/bash
-
-pip install vllm
-mkdir autodl-tmp/models
-cp -r autodl-fs/models/Qwen2.5_32B-Instruct-GPTQ-Int4 autodl-tmp/models/
-cd autodl-tmp/models/
-nohup vllm serve Qwen2.5_32B-Instruct-GPTQ-Int4 --port 6006 --gpu-memory-utilization 0.98 --max-model-len 7000 > run.log 2>&1 &
